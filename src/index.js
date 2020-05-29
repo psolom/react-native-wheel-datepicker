ed65c480afa59796ace129fee4f5ca26c8236439
David Govea <govea.d@gmail.com>
2020-05-29 15:08:55 -0700

    feat: Remove deprecated registerCustomDatePickerIOS
    BREAKING CHANGE: custom date picker functionality removed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 'use strict';
 
-import { Platform } from 'react-native';
 import Picker from './picker';
 import DatePicker from './date-picker';
 
-let DatePickerComponent = DatePicker;
-
-const registerCustomDatePickerIOS = (CustomDatePickerIOS) => {
-  if (Platform.OS === 'ios') {
-    DatePickerComponent = CustomDatePickerIOS;
-  }
-
-  return DatePickerComponent;
-};
-
-module.exports = { Picker, DatePicker: DatePickerComponent, registerCustomDatePickerIOS };
+module.exports = { Picker, DatePicker };
